20f679a0af63d5d99bc38fecd701390711e2034c
Mackery <70108834+homelikezero@users.noreply.github.com>
2022-03-22 08:59:03 -0500

    MACKERY ENGINE!!!

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -136,7 +136,10 @@ class MainMenuState extends MusicBeatState
 		versionShit.scrollFactor.set();
 		versionShit.setFormat("VCR OSD Mono", 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		add(versionShit);
-		var versionShit:FixText = new FixText(12, FixG.height - 24, 0, "Mackery Engine v" + MackeryVersion, 12);
+		var versionShit:FlxText = new FlxText(12, FlxG.height - 44, 0, "Mackery Engine v" + mackeryVersion, 12);
+		versionShit.scrollFactor.set();
+		versionShit.setFormat("VCR OSD Mono", 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+		add(versionShit);
 
 		// NG.core.calls.event.logEvent('swag').send();
 
